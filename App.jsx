4ff86a2843f3aef17f86a695887e55c8203275bc
yLevytsk <Lupin773311asd@gmail.com>
2025-02-26 09:01:08 +0000

    рп

--- App.jsx
+++ App.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React from "react"; // ✅ Отключаем правило ESLint
-/* eslint-enable no-unused-vars */
+import React from "react";
+import TransactionHistory from "./TransactionHistory";
+import transactions from "./transactions.json";
 
 function App() {
-  
+  return (
+    <div>
+      <h2>Transaction History</h2>
+      <TransactionHistory items={transactions} />
+    </div>
+  );
 }
 
 export default App;
+
 
 
 
