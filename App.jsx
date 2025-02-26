649089e0e0ba629683371dac11ad2aff5f5b39ed
yLevytsk <Lupin773311asd@gmail.com>
2025-02-26 09:15:39 +0000

    раи

--- App.jsx
+++ App.jsx
@@ -1,19 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
-import TransactionHistory from "./TransactionHistory";
-import transactions from "./transactions.json";
+import React from "react"; // ✅ Отключаем правило ESLint
+/* eslint-enable no-unused-vars */
 
 function App() {
-  return (
-    <div>
-      <h2>Transaction History</h2>
-      <TransactionHistory items={transactions} />
-    </div>
-  );
+  
 }
 
 export default App;
-
 
 
 
