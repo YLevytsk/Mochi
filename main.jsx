4ab76d839cbae8e92842d9323f2fe89dfe64f68c
yLevytsk <Lupin773311asd@gmail.com>
2025-03-02 12:09:41 +0000

    выа

--- main.jsx
+++ main.jsx
@@ -1,7 +1,6 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navigation from "./components/Navigation.jsx"; 
+import Navigation from "./components/Navigation.jsx"; // ✅ Убедись, что этот файл существует
 
 const rootElement = document.getElementById("root");
 
