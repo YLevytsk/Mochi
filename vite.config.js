507be2eaf1b51ea19396d5650332620b72c652e7
yLevytsk <Lupin773311asd@gmail.com>
2025-03-02 22:05:17 +0000

    пып

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Mochi/", // ✅ Путь для GitHub Pages
+  base: "/Mochi/", // ✅ Для GitHub Pages
   plugins: [react()],
   build: {
     outDir: "dist",
     emptyOutDir: true,
     rollupOptions: {
-      input: "index.html", // ✅ Указываем точку входа
+      input: "main.jsx", // 🔥 Указываем `main.jsx` как точку входа
       output: {
-        entryFileNames: "index.js",
+        entryFileNames: "index.js", // ✅ В dist/ создастся index.js
         chunkFileNames: "assets/[name]-[hash].js",
         assetFileNames: "assets/[name]-[hash][extname]",
       },
     },
   },
 });
+
 
 
 
