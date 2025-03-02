0397ac7b102a087cb9d13f2334e79e9a52eff196
yLevytsk <Lupin773311asd@gmail.com>
2025-03-02 11:50:39 +0000

    h

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navigation from "./components/Navigation"; 
+import Navigation from "./components/Navigation.jsx"; 
 
 const rootElement = document.getElementById("root");
 
